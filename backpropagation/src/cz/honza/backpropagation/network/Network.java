7894f12ac74f247b5a31b88bc44840c8a4a8fa5d
Jan Němec <jnem6403@seznam.cz>
2014-06-02 07:13:01 +0200

    training (under construction)

--- backpropagation/src/cz/honza/backpropagation/network/Network.java
+++ backpropagation/src/cz/honza/backpropagation/network/Network.java
@@ -3,6 +3,8 @@ package cz.honza.backpropagation.network;
 public class Network {
 	private Layer[] layers;
 	private double sumError;
+	private double[][] inputs;
+	private double[][] outputs;
 
 	static double sigma(double x) {
 		return 1.0 / (1.0 + Math.exp(-x));
@@ -42,6 +44,59 @@ public class Network {
 	}
 
 	double trainingStep() {
-		return 0;
+		 int i, j, k, l;
+		  
+	    sumError = 0;
+		    for (i = 0; i < layers.length; i++) {
+		      for (j = 0; j < layers[i].neurons.length; j++) {
+		    	  Neuron n = layers[i].neurons[j];
+		        for (k = 0; k < n.weights.length; k++) {
+		          n.weightsDerivation[k] = 0;
+		          n.moment = 0;
+		        }
+		      }
+		    }
+		    double[] output;
+		    for (i = 0; i < inputs.length; i++) {
+		      vypocet(t[i][0], vystup);
+		      for (j = 0; j < vystup.size(); j++) {
+		        sumChyba += ((vystup[j] - t[i][1][j]) * (vystup[j] - t[i][1][j]));
+		      }
+		      for (j = size() - 1; j >= 0; j--) { // backpropagation
+		        for (k = 0; k < (*this)[j].size(); k++) {
+		          if (j == size() - 1) {
+		            (*this)[j][k].derivace = (*this)[j][k].vystup - t[i][1][k];
+		          } else {
+		            (*this)[j][k].derivace = 0;
+		            for (l = 0; l < (*this)[j + 1].size(); l++) {
+		              (*this)[j][k].derivace += 
+		               (*this)[j + 1][l].derivace *
+		               (*this)[j + 1][l].vystup *
+		               (1 - (*this)[j + 1][l].vystup) *
+		               (*this)[j + 1][l][k];
+		            }
+		          }
+		          for (l = 0; l < (*this)[j][k].size(); l++) {
+		            (*this)[j][k].derivaceVah[l] +=
+		              (*this)[j][k].derivace *
+		              (*this)[j][k].vystup *
+		              (1 - (*this)[j][k].vystup) *
+		              ((l == (*this)[j][k].size() - 1) ? -1 : 
+		                (j == 0 ? t[i][0][l] : (*this)[j - 1][l].vystup));
+		          }
+		        }
+		      }
+		    }
+		    for (i = 0; i < size(); i++) {
+		      for (j = 0; j < (*this)[i].size(); j++) {
+		        for (k = 0; k < (*this)[i][j].size(); k++) {
+		          (*this)[i][j].moment = 
+		            0.01 * (*this)[i][j].derivaceVah[k] + 0.0 * (*this)[i][j].moment;
+		          (*this)[i][j][k] -= (*this)[i][j].moment;
+		        }
+		      }
+		    }
+		    sumError /= 2;
+		  return sumError;
 	}
 }
