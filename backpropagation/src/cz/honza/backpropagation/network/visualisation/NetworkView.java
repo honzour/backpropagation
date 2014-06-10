25502531502782a6c60ba602e1ee30897769c74c
Jan Němec <jnem6403@seznam.cz>
2014-06-10 07:04:54 +0200

    visualisation

--- backpropagation/src/cz/honza/backpropagation/network/visualisation/NetworkView.java
+++ backpropagation/src/cz/honza/backpropagation/network/visualisation/NetworkView.java
@@ -21,6 +21,18 @@ public class NetworkView extends View {
  	public NetworkView(Context context, AttributeSet attrs, int defStyleAttr) {
  		super(context, attrs, defStyleAttr);
  	}
+ 	
+ 	protected int getX(Network n, int layer, int neuron)
+ 	{
+ 		if (layer < 0)
+ 			return (getWidth() * (neuron + 1)) / (n.layers[0].neurons[0].weights.length);
+ 		return (getWidth() * (neuron + 1)) / (n.layers[layer].neurons.length + 1);
+ 	}
+ 	
+ 	protected int getY(Network n, int layer)
+ 	{
+ 		return (getHeight() * (layer + 1)) / (n.layers.length + 1);
+ 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
@@ -36,15 +48,40 @@ public class NetworkView extends View {
 		
 		Paint paint = new Paint();
 		
+		final int maxLayer = n.layers.length - 1;
+		// output
+		for (int j = 0; j < n.layers[maxLayer].neurons.length; j++)
+		{
+			canvas.drawLine(getX(n, maxLayer, j), height, getX(n, maxLayer, j), getY(n, maxLayer), paint);
+		}
+		
+		// input
+		for (int j = 0; j < n.layers[0].neurons.length; j++)
+		{
+			for (int l = 0; l < n.layers[0].neurons[0].weights.length - 1; l++)
+			{
+				canvas.drawLine(getX(n, 0, j), getY(n, 0), getX(n, -1, l), 0, paint);
+			}
+		}
+		
+		// synapses
+		for (int i = 1; i < n.layers.length; i++)
+		{
+			for (int j = 0; j < n.layers[i].neurons.length; j++)
+			{
+				for (int l = 0; l < n.layers[i - 1].neurons.length; l++)
+				{
+					canvas.drawLine(getX(n, i, j), getY(n, i), getX(n, i - 1, l), getY(n,i - 1), paint);
+				}
+			}	
+		}
+		
 		for (int i = 0; i < layers; i++)
 		{
-			int h = (height * (i + 1)) / (layers + 1);
-			canvas.drawLine(0, h, width, h, paint);
 			int neurons = n.layers[i].neurons.length;
 			for (int j = 0; j < neurons; j++)
 			{
-				int w = (width * (j + 1)) / (neurons + 1);
-				canvas.drawCircle(w, h, 10, paint);
+				canvas.drawCircle(getX(n, i, j), getY(n, i), (width + height) / 64, paint);
 			}
 		}
 	}
