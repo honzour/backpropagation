39fc732478df2e8c8039a1eda23f865ca0a68dc7
Jan Nemec <jnemec@acrobits.cz>
2014-06-10 12:52:31 +0200

    custom application class base

---
+++ backpropagation/src/cz/honza/backpropagation/NetworkApplication.java
@@ -0,0 +1,12 @@
+package cz.honza.backpropagation;
+
+import android.app.Application;
+
+public class NetworkApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+}
