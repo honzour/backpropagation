10ad031a2c53e58c696d3f52217f3822b6068cfe
Jan Nemec <jnemec@acrobits.cz>
2014-06-17 15:13:23 +0200

    useless code removed

--- backpropagation/src/cz/honza/backpropagation/NetworkApplication.java
+++ backpropagation/src/cz/honza/backpropagation/NetworkApplication.java
@@ -8,38 +8,4 @@ public class NetworkApplication extends Application {
 
 	public static LearningThread sThread;
 	public volatile static Network sNetwork = null;
-
-	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		if (NetworkApplication.sNetwork == null)
-		{
-			/*
-			// NOT
-			int[] anatomy = {1, 1};
-			double[][] inputs = {{0}, {1}};
-			double[][] outputs = {{1}, {0}};
-			*/
-			/*
-			// AND
-			int[] anatomy = {2, 1};
-			double[][] inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-			double[][] outputs = {{0}, {0}, {0}, {1}};
-			*/
-			/*
-			// OR
-			int[] anatomy = {2, 1};
-			double[][] inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-			double[][] outputs = {{0}, {1}, {1}, {1}};
-			*/
-			
-			// XOR
-			int[] anatomy = {2, 2, 1};
-			double[][] inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-			double[][] outputs = {{0}, {1}, {1}, {0}};
-			
-			NetworkApplication.sNetwork = new Network(anatomy, inputs, outputs);
-		}
-	}
 }
