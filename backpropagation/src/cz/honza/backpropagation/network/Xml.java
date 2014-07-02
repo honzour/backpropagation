81fb276697f4dee6b80bf3ed873f38fd50cf2960
Jan Nemec <jnemec@acrobits.cz>
2014-07-02 16:30:56 +0200

    export into xml

---
+++ backpropagation/src/cz/honza/backpropagation/network/Xml.java
@@ -0,0 +1,11 @@
+package cz.honza.backpropagation.network;
+
+public class Xml {
+	public static final String TAG_START = "<";
+	public static final String TAG_END = ">";
+	public static final String TAG_FULL_END = "/>";
+	public static final String NETWORK = "network";
+	public static final String LAYER = "layer";
+	public static final String NEURON = "neuron";
+	public static final String WEIGHT = "weight";
+}
