db4b1a903254d518085b7c89d876a7857ffa878b
Jan Nemec <jnemec@acrobits.cz>
2014-07-18 16:29:09 +0200

    export gui

--- backpropagation/src/cz/honza/backpropagation/export/ExportActivity.java
+++ backpropagation/src/cz/honza/backpropagation/export/ExportActivity.java
@@ -1,10 +1,16 @@
 package cz.honza.backpropagation.export;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import cz.honza.backpropagation.NetworkApplication;
 import cz.honza.backpropagation.R;
@@ -14,6 +20,7 @@ public class ExportActivity extends NetworkActivity {
 	
 	private View mSaveButton;
 	private View mMailButton;
+	private EditText mFileName;
 
 	private String getXml() throws IOException
 	{
@@ -43,8 +50,7 @@ public class ExportActivity extends NetworkActivity {
 		mSaveButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				showDialog(0);				
 			}
 		});
 		
@@ -57,4 +63,34 @@ public class ExportActivity extends NetworkActivity {
 		});
 
 	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		mFileName = new EditText(this);
+		final File dir = Environment.getExternalStorageDirectory();
+		String name = "network.xml";
+		if (dir != null)
+		{
+			name = dir.getAbsolutePath() + "/" + name;
+		}
+		mFileName.setText(name);
+		
+		return new AlertDialog.Builder(this)
+	    .setTitle(R.string.save)
+	    .setMessage(R.string.save_to_public_memory)
+	    .setView(mFileName)
+	    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+
+	        }
+	     })
+	    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+
+	        }
+	     })
+	    .setIcon(android.R.drawable.ic_dialog_alert).create();
+	}
+	
+	
 }
