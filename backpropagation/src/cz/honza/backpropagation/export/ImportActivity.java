fd7b7653c1482e74d192a5417c6eff4e5527b10c
Jan Nemec <jnemec@acrobits.cz>
2014-09-26 15:59:29 +0200

    improved import gui

--- backpropagation/src/cz/honza/backpropagation/export/ImportActivity.java
+++ backpropagation/src/cz/honza/backpropagation/export/ImportActivity.java
@@ -14,7 +14,6 @@ import org.w3c.dom.Text;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -29,8 +28,10 @@ public class ImportActivity extends NetworkActivity {
 	
 	private View mFileButton;
 	private View mWebButton;
+	private View mExampleButton;
 	private EditText mFileName;
 	private EditText mUrl;
+	private Spinner mExample;
 	
 	protected Node getFirstChildWithName(Node root, String name, boolean toastError)
 	{
@@ -227,103 +228,100 @@ public class ImportActivity extends NetworkActivity {
 
 	}
 	
+	protected void loadExample()
+	{
+		final int item = mExample.getSelectedItemPosition(); 
+		
+		// Identity
+		int[] id_anatomy = {1, 1};
+		double[][] id_inputs = {{0}, {1}};
+		double[][] id_outputs = {{0}, {1}};
+		
+		// NOT
+		int[] not_anatomy = {1, 1};
+		double[][] not_inputs = {{0}, {1}};
+		double[][] not_outputs = {{1}, {0}};
+						
+		// AND
+		int[] and_anatomy = {2, 1};
+		double[][] and_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+		double[][] and_outputs = {{0}, {0}, {0}, {1}};
+						
+		// OR
+		int[] or_anatomy = {2, 1};
+		double[][] or_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+		double[][] or_outputs = {{0}, {1}, {1}, {1}};
+		
+		
+		// XOR
+		int[] xor_anatomy = {2, 2, 1};
+		double[][] xor_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+		double[][] xor_outputs = {{0}, {1}, {1}, {0}};
+		
+		// Exception
+		int[] ex_anatomy = {2, 4, 1};
+		double[][] ex_inputs =  {{0, 0}, {0, 1}, {1, 0}, {1, 1}, {0.5, 0}, {0.5, 1}, {0, 0.5}, {1, 0.5},
+								  {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5} };
+		double[][] ex_outputs = {{0},    {0},    {0},    {0},    {0},      {0},      {0},      {0},
+								  {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1} };
+		
+		// Chess board
+		int[] chess_anatomy = {2, 2, 2, 1};
+		double[][] chess_inputs = 
+			{{0, 0}, {0, 0.4}, {0.4, 0}, {0.4, 0.4},
+			 {0.6, 0.6}, {0.6, 1}, {1, 0.6}, {1, 1},
+			 {0.6, 0}, {1, 0}, {1, 0.4}, {0.6, 0.4},
+			 {0, 0.6}, {0.4, 1}, {0.4, 0.6}, {0, 1}
+			};
+		double[][] chess_outputs =
+			{{0}, {0}, {0}, {0},
+			 {0}, {0}, {0}, {0},
+			 {1}, {1}, {1}, {1},
+			 {1}, {1}, {1}, {1}						
+			};
+		
+		
+		switch (item)
+		{
+		case 0:
+			NetworkApplication.sNetwork = new Network(id_anatomy, new TrainingSet(id_inputs, id_outputs));
+			break;
+		case 1:
+			NetworkApplication.sNetwork = new Network(not_anatomy, new TrainingSet(not_inputs, not_outputs));
+			break;
+		case 2:
+			NetworkApplication.sNetwork = new Network(and_anatomy, new TrainingSet(and_inputs, and_outputs));
+			break;
+		case 3:
+			NetworkApplication.sNetwork = new Network(or_anatomy, new TrainingSet(or_inputs, or_outputs));
+			break;
+		case 4:
+			NetworkApplication.sNetwork = new Network(xor_anatomy, new TrainingSet(xor_inputs, xor_outputs));
+			break;
+		case 5:
+			NetworkApplication.sNetwork = new Network(ex_anatomy, new TrainingSet(ex_inputs, ex_outputs));
+			break;
+		case 6:
+			NetworkApplication.sNetwork = new Network(chess_anatomy, new TrainingSet(chess_inputs, chess_outputs));
+			break;
+
+		}
+		finish();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.import_xml);
 
-		Spinner problem = (Spinner) findViewById(R.id.import_new_task);
-		problem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+		mExample = (Spinner) findViewById(R.id.import_new_task);
+		mExampleButton = findViewById(R.id.import_examle);
+		mExampleButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int item, long arg3) {
-				
-				if (item == 0)
-					return;
-				
-				// Identity
-				int[] id_anatomy = {1, 1};
-				double[][] id_inputs = {{0}, {1}};
-				double[][] id_outputs = {{0}, {1}};
-				
-				// NOT
-				int[] not_anatomy = {1, 1};
-				double[][] not_inputs = {{0}, {1}};
-				double[][] not_outputs = {{1}, {0}};
-								
-				// AND
-				int[] and_anatomy = {2, 1};
-				double[][] and_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-				double[][] and_outputs = {{0}, {0}, {0}, {1}};
-								
-				// OR
-				int[] or_anatomy = {2, 1};
-				double[][] or_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-				double[][] or_outputs = {{0}, {1}, {1}, {1}};
-				
-				
-				// XOR
-				int[] xor_anatomy = {2, 2, 1};
-				double[][] xor_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-				double[][] xor_outputs = {{0}, {1}, {1}, {0}};
-				
-				// Exception
-				int[] ex_anatomy = {2, 4, 1};
-				double[][] ex_inputs =  {{0, 0}, {0, 1}, {1, 0}, {1, 1}, {0.5, 0}, {0.5, 1}, {0, 0.5}, {1, 0.5},
-										  {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5} };
-				double[][] ex_outputs = {{0},    {0},    {0},    {0},    {0},      {0},      {0},      {0},
-										  {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1} };
-				
-				// Chess board
-				int[] chess_anatomy = {2, 2, 2, 1};
-				double[][] chess_inputs = 
-					{{0, 0}, {0, 0.4}, {0.4, 0}, {0.4, 0.4},
-					 {0.6, 0.6}, {0.6, 1}, {1, 0.6}, {1, 1},
-					 {0.6, 0}, {1, 0}, {1, 0.4}, {0.6, 0.4},
-					 {0, 0.6}, {0.4, 1}, {0.4, 0.6}, {0, 1}
-					};
-				double[][] chess_outputs =
-					{{0}, {0}, {0}, {0},
-					 {0}, {0}, {0}, {0},
-					 {1}, {1}, {1}, {1},
-					 {1}, {1}, {1}, {1}						
-					};
-				
-				
-				switch (item)
-				{
-				case 1:
-					NetworkApplication.sNetwork = new Network(id_anatomy, new TrainingSet(id_inputs, id_outputs));
-					break;
-				case 2:
-					NetworkApplication.sNetwork = new Network(not_anatomy, new TrainingSet(not_inputs, not_outputs));
-					break;
-				case 3:
-					NetworkApplication.sNetwork = new Network(and_anatomy, new TrainingSet(and_inputs, and_outputs));
-					break;
-				case 4:
-					NetworkApplication.sNetwork = new Network(or_anatomy, new TrainingSet(or_inputs, or_outputs));
-					break;
-				case 5:
-					NetworkApplication.sNetwork = new Network(xor_anatomy, new TrainingSet(xor_inputs, xor_outputs));
-					break;
-				case 6:
-					NetworkApplication.sNetwork = new Network(ex_anatomy, new TrainingSet(ex_inputs, ex_outputs));
-					break;
-				case 7:
-					NetworkApplication.sNetwork = new Network(chess_anatomy, new TrainingSet(chess_inputs, chess_outputs));
-					break;
-
-				}
-				finish();
+			public void onClick(View v) {
+				loadExample();
 			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
-			
 		});
-
 		
 		
 		mFileButton = findViewById(R.id.import_load);
