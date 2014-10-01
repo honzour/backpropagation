383fec55a75486726a04e3eaecaecd1624fa49bf
Jan Nemec <jnemec@acrobits.cz>
2014-10-01 13:50:11 +0200

    parser to an extra file

--- backpropagation/src/cz/honza/backpropagation/export/ImportActivity.java
+++ backpropagation/src/cz/honza/backpropagation/export/ImportActivity.java
@@ -3,19 +3,8 @@ package cz.honza.backpropagation.export;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -23,8 +12,8 @@ import android.widget.Toast;
 import cz.honza.backpropagation.NetworkApplication;
 import cz.honza.backpropagation.R;
 import cz.honza.backpropagation.network.Network;
+import cz.honza.backpropagation.network.Parser;
 import cz.honza.backpropagation.network.TrainingSet;
-import cz.honza.backpropagation.network.Xml;
 import cz.honza.backpropagation.util.NetworkActivity;
 
 public class ImportActivity extends NetworkActivity {
@@ -35,199 +24,7 @@ public class ImportActivity extends NetworkActivity {
 	private EditText mFileName;
 	private EditText mUrl;
 	private Spinner mExample;
-	
-	protected Node getFirstChildWithName(Node root, String name /*, boolean toastError*/)
-	{
-		NodeList nodes = root.getChildNodes();
-		final int length = nodes.getLength();
-		for (int i = 0; i < length; i++)
-		{
-			 final Node n = nodes.item(i);
-			 final String s = n.getNodeName();
-			 if (s != null && s.equals(name))
-			 {
-				 return n;
-			 }
-		}
-		/*
-		if (toastError)
-		{
-			Toast.makeText(ImportActivity.this, "No " + name + " tag", Toast.LENGTH_LONG).show();
-		}
-		*/
-		return null;
-	}
-	
-	protected List<Double> parseNumbers(Node node)
-	{
-		final List<Double> result = new ArrayList<Double>();
-		final NodeList numbers = node.getChildNodes();
-		final int numbersCount = numbers.getLength();
-		for (int j = 0; j < numbersCount; j++)
-		{
-			final Node number = numbers.item(j);
-			if (!number.getNodeName().equals(Xml.NUMBER))
-				continue;
-			Double value;
-			try {
-				Node data = number.getChildNodes().item(0);
-				value = Double.valueOf(((Text)data).getData());
-			} catch (Exception e)
-			{
-				value = null;
-			}
-			
-			result.add(value);
-		}
-		return result;
-	}
-	
-	protected List<List<List<Double>>> parseTraining(Node network) {
-		final Node trainingNode = getFirstChildWithName(network, Xml.TRAINING /*, true*/);
-		if (trainingNode == null)
-		{
-			return null;
-		}
-		
-		final List<List<List<Double>>> result = new ArrayList<List<List<Double>>>();
-		final List<List<Double>> inputList = new ArrayList<List<Double>>();
-		final List<List<Double>> outputList = new ArrayList<List<Double>>();
-		result.add(inputList);
-		result.add(outputList);
-		
-		final Node inputsNode = getFirstChildWithName(trainingNode, Xml.INPUTS /*, true */);
-		if (inputsNode != null)
-		{
-			final NodeList inputs = inputsNode.getChildNodes();
-			final int inputsCount = inputs.getLength();
-			for (int i = 0; i < inputsCount; i++)
-			{
-				final Node inputNode = inputs.item(i);
-				if (!inputNode.getNodeName().equals(Xml.INPUT))
-					continue;
-				inputList.add(parseNumbers(inputNode));
-			}
-		}
-		
-		final Node outputsNode = getFirstChildWithName(trainingNode, Xml.OUTPUTS /*, true*/);
-		if (outputsNode != null)
-		{
-			final NodeList outputs = outputsNode.getChildNodes();
-			final int outputsCount = outputs.getLength();
-			for (int i = 0; i < outputsCount; i++)
-			{
-				final Node outputNode = outputs.item(i);
-				if (!outputNode.getNodeName().equals(Xml.OUTPUT))
-					continue;
-				outputList.add(parseNumbers(outputNode));
-			}
-		}
-		
-		return result;
-	}
-	
-	protected List<List<List<Double>>> parseLayers(Node network) {
-		final Node layersNode = getFirstChildWithName(network, Xml.LAYERS /*, true*/);
 
-		if (layersNode == null)
-		{
-			return null;
-		}
-		
-		NodeList layers = layersNode.getChildNodes();
-
-		final List<List<List<Double>>> layersData = new ArrayList<List<List<Double>>>();
-		
-		final int layersCount = layers.getLength();
-		
-		for (int i = 0; i < layersCount; i++)
-		{
-			final Node layerNode = layers.item(i);
-			String name = layerNode.getNodeName();
-			if (name == null || !name.equals(Xml.LAYER))
-				continue;
-			
-			final List<List<Double>> layerData = new ArrayList<List<Double>>();
-			
-			final NodeList neurons = layerNode.getChildNodes();
-			final int neuronsCount = neurons.getLength();
-			for (int j = 0; j < neuronsCount; j++)
-			{
-				final Node neuronNode = neurons.item(j);
-				name = neuronNode.getNodeName();
-				if (name == null || !name.equals(Xml.NEURON))
-					continue;
-				
-				final List<Double> neuronData = new ArrayList<Double>();
-				
-				final NodeList weights = neuronNode.getChildNodes();
-				final int weightsCount = weights.getLength();
-				
-				for (int k = 0; k < weightsCount; k++)
-				{
-					final Node weightNode = weights.item(k);
-					name = weightNode.getNodeName();
-					if (name == null || !name.equals(Xml.WEIGHT))
-						continue;
-				
-					
-					Double val = null;
-					
-					try {
-						Node data = weightNode.getChildNodes().item(0);
-						val = Double.valueOf(((Text)data).getData());
-					} catch (Exception e)
-					{
-					}
-					
-					neuronData.add(val);
-				}
-				
-				layerData.add(neuronData);							
-			}
-			
-			layersData.add(layerData);
-		}
-		return layersData;
-	}
-	
-	
-	protected void parseXml(InputStream is, Handler h) throws Exception
-	{
-		
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		
-		Document doc = dBuilder.parse(is);
-		doc.getDocumentElement().normalize();
-
-		final Node network = getFirstChildWithName(doc, Xml.NETWORK /*, true*/);
-		if (network == null)
-		{
-			return;
-		}
-			
-		final List<List<List<Double>>> layers = parseLayers(network);
-		if (layers == null)
-			return;
-			
-		final List<List<List<Double>>> training = parseTraining(network);
-		if (training == null)
-			return;
-			
-		Network networkTmp = new Network(layers, training);
-		// TODO check
-		NetworkApplication.sNetwork = networkTmp;
-
-		h.post(new Runnable() {
-			@Override
-			public void run() {
-				finish();
-			}
-		});
-
-	}
-	
 	protected void loadExample()
 	{
 		final int item = mExample.getSelectedItemPosition(); 
@@ -342,7 +139,6 @@ public class ImportActivity extends NetworkActivity {
 					Toast.makeText(ImportActivity.this, R.string.enter_url_first, Toast.LENGTH_LONG).show();
 					return;
 				}
-				final Handler h = new Handler();
 				Thread t = new Thread(new Runnable() {
 					@Override
 					public void run() {
@@ -350,16 +146,17 @@ public class ImportActivity extends NetworkActivity {
 						{
 							URL u = new URL(url);
 							final InputStream inputStream = u.openStream();
-							parseXml(inputStream, h);
+							NetworkApplication.sNetwork = Parser.parseXml(inputStream);
 						}
 						catch (final Throwable e)
 						{
+							/*
 							h.post(new Runnable() {
 								public void run() {
 									Toast.makeText(ImportActivity.this, e.toString(), Toast.LENGTH_LONG).show();
 								}
 							});
-							
+							*/
 						}
 					}
 				});
@@ -379,9 +176,8 @@ public class ImportActivity extends NetworkActivity {
 				}
 				try
 				{
-					final Handler h = new Handler();
 					final InputStream inputStream = new FileInputStream(filename);
-					parseXml(inputStream, h);
+					NetworkApplication.sNetwork = Parser.parseXml(inputStream);
 				}
 				catch (Throwable e)
 				{
---
+++ backpropagation/src/cz/honza/backpropagation/network/Parser.java
@@ -0,0 +1,200 @@
+package cz.honza.backpropagation.network;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+
+public class Parser {
+	public static Network parseXml(InputStream is) throws Exception
+	{
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		
+		Document doc = dBuilder.parse(is);
+		doc.getDocumentElement().normalize();
+
+		final Node network = getFirstChildWithName(doc, Xml.NETWORK /*, true*/);
+		if (network == null)
+		{
+			return null;
+		}
+			
+		final List<List<List<Double>>> layers = parseLayers(network);
+		if (layers == null)
+			return null;
+			
+		final List<List<List<Double>>> training = parseTraining(network);
+		if (training == null)
+			return null;
+			
+		Network networkTmp = new Network(layers, training);
+		// TODO check
+		return networkTmp;
+
+	}
+	
+	protected static Node getFirstChildWithName(Node root, String name /*, boolean toastError*/)
+	{
+		NodeList nodes = root.getChildNodes();
+		final int length = nodes.getLength();
+		for (int i = 0; i < length; i++)
+		{
+			 final Node n = nodes.item(i);
+			 final String s = n.getNodeName();
+			 if (s != null && s.equals(name))
+			 {
+				 return n;
+			 }
+		}
+		/*
+		if (toastError)
+		{
+			Toast.makeText(ImportActivity.this, "No " + name + " tag", Toast.LENGTH_LONG).show();
+		}
+		*/
+		return null;
+	}
+
+	protected static List<List<List<Double>>> parseTraining(Node network) {
+		final Node trainingNode = getFirstChildWithName(network, Xml.TRAINING /*, true*/);
+		if (trainingNode == null)
+		{
+			return null;
+		}
+		
+		final List<List<List<Double>>> result = new ArrayList<List<List<Double>>>();
+		final List<List<Double>> inputList = new ArrayList<List<Double>>();
+		final List<List<Double>> outputList = new ArrayList<List<Double>>();
+		result.add(inputList);
+		result.add(outputList);
+		
+		final Node inputsNode = getFirstChildWithName(trainingNode, Xml.INPUTS /*, true */);
+		if (inputsNode != null)
+		{
+			final NodeList inputs = inputsNode.getChildNodes();
+			final int inputsCount = inputs.getLength();
+			for (int i = 0; i < inputsCount; i++)
+			{
+				final Node inputNode = inputs.item(i);
+				if (!inputNode.getNodeName().equals(Xml.INPUT))
+					continue;
+				inputList.add(parseNumbers(inputNode));
+			}
+		}
+		
+		final Node outputsNode = getFirstChildWithName(trainingNode, Xml.OUTPUTS /*, true*/);
+		if (outputsNode != null)
+		{
+			final NodeList outputs = outputsNode.getChildNodes();
+			final int outputsCount = outputs.getLength();
+			for (int i = 0; i < outputsCount; i++)
+			{
+				final Node outputNode = outputs.item(i);
+				if (!outputNode.getNodeName().equals(Xml.OUTPUT))
+					continue;
+				outputList.add(parseNumbers(outputNode));
+			}
+		}
+		
+		return result;
+	}
+	
+	protected static List<List<List<Double>>> parseLayers(Node network) {
+		final Node layersNode = getFirstChildWithName(network, Xml.LAYERS /*, true*/);
+
+		if (layersNode == null)
+		{
+			return null;
+		}
+		
+		NodeList layers = layersNode.getChildNodes();
+
+		final List<List<List<Double>>> layersData = new ArrayList<List<List<Double>>>();
+		
+		final int layersCount = layers.getLength();
+		
+		for (int i = 0; i < layersCount; i++)
+		{
+			final Node layerNode = layers.item(i);
+			String name = layerNode.getNodeName();
+			if (name == null || !name.equals(Xml.LAYER))
+				continue;
+			
+			final List<List<Double>> layerData = new ArrayList<List<Double>>();
+			
+			final NodeList neurons = layerNode.getChildNodes();
+			final int neuronsCount = neurons.getLength();
+			for (int j = 0; j < neuronsCount; j++)
+			{
+				final Node neuronNode = neurons.item(j);
+				name = neuronNode.getNodeName();
+				if (name == null || !name.equals(Xml.NEURON))
+					continue;
+				
+				final List<Double> neuronData = new ArrayList<Double>();
+				
+				final NodeList weights = neuronNode.getChildNodes();
+				final int weightsCount = weights.getLength();
+				
+				for (int k = 0; k < weightsCount; k++)
+				{
+					final Node weightNode = weights.item(k);
+					name = weightNode.getNodeName();
+					if (name == null || !name.equals(Xml.WEIGHT))
+						continue;
+					
+					Double val = null;
+					
+					try {
+						Node data = weightNode.getChildNodes().item(0);
+						val = Double.valueOf(((Text)data).getData());
+					} catch (Exception e)
+					{
+						// TODO
+					}
+					
+					neuronData.add(val);
+				}
+				
+				layerData.add(neuronData);							
+			}
+			
+			layersData.add(layerData);
+		}
+		return layersData;
+	}
+
+	protected static List<Double> parseNumbers(Node node)
+	{
+		final List<Double> result = new ArrayList<Double>();
+		final NodeList numbers = node.getChildNodes();
+		final int numbersCount = numbers.getLength();
+		for (int j = 0; j < numbersCount; j++)
+		{
+			final Node number = numbers.item(j);
+			if (!number.getNodeName().equals(Xml.NUMBER))
+				continue;
+			Double value;
+			try {
+				Node data = number.getChildNodes().item(0);
+				value = Double.valueOf(((Text)data).getData());
+			} catch (Exception e)
+			{
+				// TODO
+				value = null;
+			}
+			
+			result.add(value);
+		}
+		return result;
+	}
+}
