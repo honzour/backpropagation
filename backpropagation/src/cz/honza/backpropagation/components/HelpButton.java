b73ba9d9db388e728e9f21de00ccd2f0d2046ee4
Jan Nemec <jnemec@acrobits.cz>
2014-11-13 09:55:12 +0100

    HelpButton class

---
+++ backpropagation/src/cz/honza/backpropagation/components/HelpButton.java
@@ -0,0 +1,23 @@
+package cz.honza.backpropagation.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class HelpButton extends Button {
+
+	protected String mUrl;
+	
+	public HelpButton(Context context) {
+		super(context);
+	}
+	
+	public HelpButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public HelpButton(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+}
