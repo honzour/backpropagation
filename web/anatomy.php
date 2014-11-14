d69489ba50446d0262c6d68bdebb12695d0808b0
Jan Nemec <jnemec@acrobits.cz>
2014-11-14 15:36:50 +0100

    missing help php files

---
+++ web/anatomy.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation - Network anatomy</TITLE>
+</HEAD>
+<BODY>
+<?
+include 'google.php'
+?>
+<H2>Network anatomy</H2>
+<P>
+</P>
+
+</BODY>
+</HTML>
+
---
+++ web/trainingset.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation - Training set</TITLE>
+</HEAD>
+<BODY>
+<?
+include 'google.php'
+?>
+<H2>Training set</H2>
+<P>
+</P>
+
+</BODY>
+</HTML>
+
