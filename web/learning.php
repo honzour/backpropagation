65d3535de535470dd85ab3cc97724945880a1164
Jan Nemec <honza@debian.honza>
2014-11-16 03:37:51 +0100

    error in english

--- web/learning.php
+++ web/learning.php
@@ -12,7 +12,7 @@ include 'google.php'
 When you load an example or create your own task in the editor, it will have all connections and tresholds weights set to some small random number near zero. This means that the network is not trained and it will fail even on the training set examples. Learning means adapting all weights to get the minimal medium quadratic error on training set exaples. If we succseed, error on training set will be very small and we will hope that it is because the network got some hidden rules from training set data so it will get good result for each inout, not just training set elements. Adapting  is beeing done by the backpropagation algorithm. It works in steps and we calculate derivation of training set error per each weight and move the weights vector to the right direction. Algorithm goes from the last layer to the first so it is called backpropagation. There is a learning speed parameter called 'alpha'. If you set it to a big number, the algorithm will move the weights in each step by a big number so it will go much faster in clear and simple cases, but it can divergate in complex tasks. You can stop learning by 'Stop' button or leaving this screen by a back key when the error is small enough.
 </P>
 <P>
-If you want to know more about this algorithm, I will recomand you <A HREF="https://www.coursera.org/course/neuralnets">this free coursera course</A>.
+If you want to know more about this algorithm, I will recommend you <A HREF="https://www.coursera.org/course/neuralnets">this free coursera course</A>.
 </P>
 <P>
 If the learning is not successful you can try
