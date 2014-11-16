a25c0c8fa0c96d5ef5002cdbf3427a3e89c1c709
Jan Nemec <honza@debian.honza>
2014-11-16 04:31:13 +0100

    editor and anatomy

--- web/anatomy.php
+++ web/anatomy.php
@@ -9,6 +9,7 @@ include 'google.php'
 ?>
 <H2>Network anatomy</H2>
 <P>
+You can edit the network anatomy here. Please note that one hidden layer is just enough for a basic backpropagation algorithm but feel free to experiment with deeper networks. Press '+' and '-' to change the neuron count, 'x' to delete the layer or 'Add layer' to add a new layer to the end.
 </P>
 
 </BODY>
