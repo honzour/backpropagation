939e20e85642db7d590362b43e0207b41327d4e5
Jan Nemec <honza@debian.honza>
2014-11-16 04:39:35 +0100

    training set

--- web/trainingset.php
+++ web/trainingset.php
@@ -9,6 +9,7 @@ include 'google.php'
 ?>
 <H2>Training set</H2>
 <P>
+Training set is simply a set of pairs (input x, expected output for input x). The current version of algorithm in this program does not work well if the numers from input are to large. If you want to get better result, please scale input to <I>&lt;-1, 1&gt;<SUP>n</SUP></I> where <I>n</I> is your input dimension. There is no multiplying layer at the end of the network so the output is always from <I>&lt;0, 1&gt;<SUP>m</SUP></I>. So output must be set by chackboxed where checked means 1 and unchecked 0. I have fixing of both that issues on my TODO list.
 </P>
 
 </BODY>
