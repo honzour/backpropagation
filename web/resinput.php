d119fb010219a9ee6e71a05ffbd866989d5e1389
Jan Nemec <honza@debian.honza>
2014-11-16 04:00:38 +0100

    resinput

--- web/resinput.php
+++ web/resinput.php
@@ -9,6 +9,7 @@ include 'google.php'
 ?>
 <H2>Result for input</H2>
 <P>
+If you want to see result of your network for any exact input, simply type your inpout vector and the network will calculate the output. The result is updated each time you change any inpout character.
 </P>
 
 </BODY>
