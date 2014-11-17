a467309e207dda39f4a4af2fce071fb131634f2e
Jan Nemec <honza@debian.honza>
2014-11-17 01:04:51 +0100

    fixed some missprints

--- web/editor.php
+++ web/editor.php
@@ -14,12 +14,12 @@ Welcome to the neural network editor screen. This screen always opens the curren
 
 <H3>Edit anatomy</H3>
 <P>
-Basic backpropagation cannot train deep networks. So it normaly sucseed when there is just single hidden layer. The left most number is the input dimension, medium numbers are hidden layers neuron counts and the right most number is neuron count in the last layer which is also the outpout dimension. Press the button to edit it.
+Basic backpropagation cannot train deep networks. So it normaly succeed when there is just single hidden layer. The left most number is the input dimension, medium numbers are hidden layers neuron counts and the right most number is neuron count in the last layer which is also the outpout dimension. Press the button to edit it.
 </P>
 
 <H3>Edit training set</H3>
 <P>
-Training set is a set of examples with an input and expected output. For example if you want to teach your network prime numbers there will be for example (1)-&gt;(0), (2)-&gt;(1), (3)-&gt;(1), (4)-&gt;(0), (5)-&gt;(1), (6)-&gt;(0) and (8)-&gt;(0) and you will hope that after training, yor network will calculate not only this set well but it will also for example it will give for input 7 result 1. (Adn your network will fail because this is to complex problem :-))
+Training set is a set of examples with an input and expected output. For example if you want to teach your network prime numbers there will be for example (1)-&gt;(0), (2)-&gt;(1), (3)-&gt;(1), (4)-&gt;(0), (5)-&gt;(1), (6)-&gt;(0) and (8)-&gt;(0) and you will hope that after training, yor network will calculate not only this set well but it will also for example give for input 7 result 1. (And your network will fail because this is to complex problem :-))
 </P>
 
 </BODY>
