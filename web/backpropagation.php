4d5e6758b837bfee48b852a2a43039e9486d0fe1
Jan Nemec <jnemec@acrobits.cz>
2014-11-18 10:07:33 +0100

    algorithm description

---
+++ web/backpropagation.php
@@ -0,0 +1,30 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation algorithm</TITLE>
+</HEAD>
+<BODY>
+<?
+include 'google.php'
+?>
+<H2>Backpropagation algorithm</H2>
+<P>
+This is just a basic introduction to artifitial neural networks and backpropagation algorithm. Large networks trained by this algorithm (with many improvements) can do lot of very interesting stuff. They can detect some object in a picture, make a weather forecast, predict the next word in a text etc.
+</P>
+<H3>Perceptron</H3>
+<P>
+</P>
+<H3>Neural network</H3>
+<P>
+</P>
+<H3>Problem with learning</H3>
+<P>
+</P>
+<H3>Gradient method</H3>
+<P>
+</P>
+<H3>Backpropagation</H3>
+<P>
+</P>
+</BODY>
+</HTML>
