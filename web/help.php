9d7bdfbf323e639606b5bf03fa71ef2c46ce32cb
Jan Nemec <honza@debian.honza>
2014-11-26 19:57:44 +0100

    TODO and help update

--- web/help.php
+++ web/help.php
@@ -14,15 +14,21 @@ Do you like this program? And do you want to help me to develop it? There are ma
 <UL>
 	<LI>You are a common person without any knowledge and without money
 		<UL>
-			<LI>Rate this app in Google Play. Short comment is better than just start.</LI>
+			<LI><A HREF="market://details?id=cz.honza.backpropagation">Rate this app</A> in Google Play. Short comment is better than just start.</LI>
 			<LI>Tell your friends about this app</LI>
 			<LI>Share this app on your favorite social network</LI>
+		</UL>
+	</LI>
+	<LI>You are a journalist or an owner of related www pages
+		<UL>
+			<LI>Write an article about it</LI>
+			<LI>Add a link to <A HREF="http://backpropagation.moxo.cz/">http://backpropagation.moxo.cz/</A></LI>
 		</UL>
 	</LI>
 	<LI>You are a native English speaker
 		<UL>
 			<LI>Fix my poor English on this www</LI>
-			<LI>Fix my poor English on in the program resources</LI>
+			<LI>Fix my poor English on <A HREF="https://github.com/honzour/backpropagation/blob/master/backpropagation/res/values/strings.xml">in the program resources</A></LI>
 		</UL>
 	</LI>
 	<LI>You are a native speaker of other language then Czech or English
@@ -35,18 +41,24 @@ Do you like this program? And do you want to help me to develop it? There are ma
 			<LI>Explain me how to teach the network the chessboard problem from the hardcoded examples. It always finds just a local minimum :-(</LI>
 		</UL>
 	</LI>
+	<LI>You are a tester and have found a bug
+		<UL>
+			<LI>Please report it to me. In case of a crash, <A HREF="http://developer.android.com/tools/help/logcat.html">LogCat</A> would help me a lot. Please report the current version issues only.</LI>
+		</UL>
+	</LI>
 	<LI>You have any great idea how to improve this program
 		<UL>
-			<LI>Write it to me. I cannot promiss anything but at least I will read it and know what users want.</LI>
+			<LI>Write it to me. I cannot promise anything but at least I will read it and know what users want.</LI>
 		</UL>
 	</LI>
 	<LI>You are a rich person
 		<UL>
 			<LI>Send me some money by paypal (jnem6403 (at) seznam (dot) cz) just to motivate me to a better work.</LI>
 			<LI>Send me some money for creating a sponsor screen in the app with your name at the first line.</LI>
-			<LI>Send me some money your add in this app.</LI>
+			<LI>Send me some money for your add in this app.</LI>
 			<LI>Send me some money for implementing some feature in the <A HREF="todo.php">TODO list</A> or some another.</LI>
 		</UL>
+		The program has (according to Google Play) between 90 and 100 active users and I am sure most of them are very smart people.
 	</LI>
 </UL>
 
