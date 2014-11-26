5ac62cc867173b278a62bd369720d404856a492e
Jan Nemec <honza@debian.honza>
2014-11-26 03:39:32 +0100

    scale description

--- web/trainingset.php
+++ web/trainingset.php
@@ -9,8 +9,7 @@ include 'google.php'
 ?>
 <H2>Training set</H2>
 <P>
-Training set is simply a set of pairs (input x, expected output for input x). The current version of algorithm in this program does not work well if the numbers from input are to large. If you want to get better result, please scale input to <I>&lt;-1, 1&gt;<SUP>n</SUP></I> where <I>n</I> is your input dimension. There is no multiplying layer at the end of the network so the output is always from <I>&lt;0, 1&gt;<SUP>m</SUP></I>. So output must be set by checkboxes where checked means 1 and unchecked 0. I have fixing of both that issues on my TODO list.
-</P>
+Training set is simply a set of pairs (input x, expected output for input x). Feel free to type any real numbers but input will be scaled to <I>&lt;-1, 1&gt;<SUP>m</SUP></I> (it is better for the algorithm) and output to <I>&lt;0, 1&gt;<SUP>m</SUP></I> (neuron output is always between 0 and 1). It will be rescaled during the computing back so you will always work with your original numbers.
 <?
 include 'links.php'
 ?>
