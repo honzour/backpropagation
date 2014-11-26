775c57eeeb435ac4bb380ad653ff8f309c6699da
Jan Nemec <honza@debian.honza>
2014-11-26 11:10:12 +0100

    how to help

---
+++ web/help.php
@@ -0,0 +1,58 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation - How to help</TITLE>
+</HEAD>
+<BODY>
+<?
+include 'google.php'
+?>
+<H2>How to help</H2>
+<P>
+Do you like this program? And do you want to help me to develop it? There are many ways how to help. Feel free to contact me at jnem6403 (at) seznam (dot) cz.
+</P>
+<UL>
+	<LI>You are a common person without any knowledge and without money
+		<UL>
+			<LI>Rate this app in Google Play. Short comment is better than just start.</LI>
+			<LI>Tell your friends about this app</LI>
+			<LI>Share this app on your favorite social network</LI>
+		</UL>
+	</LI>
+	<LI>You are a native English speaker
+		<UL>
+			<LI>Fix my poor English on this www</LI>
+			<LI>Fix my poor English on in the program resources</LI>
+		</UL>
+	</LI>
+	<LI>You are a native speaker of other language then Czech or English
+		<UL>
+			<LI>Translate this program into your language (Portugese, Italian, Russian, ...)</LI>
+		</UL>
+	</LI>
+	<LI>You are a neural network expert
+		<UL>
+			<LI>Explain me how to teach the network the chessboard problem from the hardcoded examples. It always finds just a local minimum :-(</LI>
+		</UL>
+	</LI>
+	<LI>You have any great idea how to improve this program
+		<UL>
+			<LI>Write it to me. I cannot promiss anything but at least I will read it and know what users want.</LI>
+		</UL>
+	</LI>
+	<LI>You are a rich person
+		<UL>
+			<LI>Send me some money by paypal (jnem6403 (at) seznam (dot) cz) just to motivate me to a better work.</LI>
+			<LI>Send me some money for creating a sponsor screen in the app with your name at the first line.</LI>
+			<LI>Send me some money your add in this app.</LI>
+			<LI>Send me some money for implementing some feature in the <A HREF="todo.php">TODO list</A> or some another.</LI>
+		</UL>
+	</LI>
+</UL>
+
+<?
+include 'links.php'
+?>
+</BODY>
+</HTML>
+
