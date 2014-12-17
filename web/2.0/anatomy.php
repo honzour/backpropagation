c4cc073190eb49d5d9913a316e32acf999ea30b3
Jan Nemec <honza@debian.honza>
2014-12-17 06:53:15 +0100

    help update

--- web/2.0/anatomy.php
+++ web/2.0/anatomy.php
@@ -9,7 +9,7 @@ include 'google.php'
 ?>
 <H2>Network anatomy</H2>
 <P>
-You can edit the network anatomy here. Please note that one hidden layer is just enough for a basic backpropagation algorithm but feel free to experiment with deeper networks. Press '+' and '-' to change the neuron count, 'x' to delete the layer or 'Add layer' to add a new layer to the end.
+You can edit the network anatomy here. The first layer is just an input so it is not a real neuron layer. The last layer is an output neuron layer, so the first and the last layer size must be exactly equal to the input and output dimension. Please note that one hidden layer is just enough for a basic backpropagation algorithm. Feel free to experiment with deeper networks but do not expect good results. Press '+' and '-' to change the neuron count, 'x' to delete the layer or 'Add layer' to add a new layer to the end.
 </P>
 <?
 include 'links.php'
