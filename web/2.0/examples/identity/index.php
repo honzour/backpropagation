b535570987e294532aea74fc8eeb6eecb621860b
Jan Nemec <jnemec@acrobits.cz>
2015-01-06 10:23:40 +0100

    the first example on www

---
+++ web/2.0/examples/identity/index.php
@@ -0,0 +1,18 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation - Examples - Identity</TITLE>
+</HEAD>
+<BODY>
+<?
+include '../../google.php'
+?>
+<P>
+<A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.csv">Import the problem to be learned</A><BR>
+<A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.xml">Import the solution</A><BR>
+</P>
+<?
+include '../../links.php'
+?>
+</BODY>
+</HTML>
---
+++ web/2.0/examples/index.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation - Examples</TITLE>
+</HEAD>
+<BODY>
+<?
+include '../google.php'
+?>
+<P>
+<A HREF="identity">Identity</A><BR>
+</P>
+<?
+include '../links.php'
+?>
+</BODY>
+</HTML>
