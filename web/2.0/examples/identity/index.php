3f48d34b67a4724c3b083b7f39428568aa9c52c8
Jan Nemec <jnemec@acrobits.cz>
2015-01-06 17:09:23 +0100

    identity imeges

--- web/2.0/examples/identity/index.php
+++ web/2.0/examples/identity/index.php
@@ -7,10 +7,20 @@
 <?
 include '../../google.php'
 ?>
+<H2>Identity</H2>
 <P>
-<A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.csv">Import the problem to be learned</A><BR>
-<A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.xml">Import the solution</A><BR>
+A very basic hello world example. We simply want the network to return 0 for input 0 and 1 for input 1. Single layer network with just one neuron is used and even this is quite overkill for this task.
 </P>
+<UL>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.csv">Import the problem to be learned</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.xml">Import the solution</A></LI>
+<LI><A HREF="network.csv">View the problem CSV file</A></LI>
+<LI><A HREF="network.xml">View the solution XML file</A></LI>
+<LI><BR><IMG SRC="result.png">
+<BR>Result visualisation</LI>
+<LI><BR><IMG SRC="network.png">
+<BR>Network visualisation</LI>
+</UL>
 <?
 include '../../links.php'
 ?>
