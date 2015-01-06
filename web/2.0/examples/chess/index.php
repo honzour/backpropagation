b42e59d58d6c2e529b41d0a1636b4cc44d950a01
Jan Nemec <honza@debian.honza>
2015-01-06 22:06:14 +0100

    chess problem

--- web/2.0/examples/chess/index.php
+++ web/2.0/examples/chess/index.php
@@ -1,19 +1,20 @@
 <HTML>
 <HEAD>
 <META charset="utf-8">
-<TITLE>Backpropagation - Examples - Identity</TITLE>
+<TITLE>Backpropagation - Examples - Chess</TITLE>
 </HEAD>
 <BODY>
 <?
 include '../../google.php'
 ?>
-<H2>Identity</H2>
+<H2>Chess 2x2</H2>
 <P>
-A very basic hello world example. We simply want the network to return 0 for input 0 and 1 for input 1. Single layer network with just one neuron is used and even this is quite overkill for this task.
+We have a chess board 2x2 and we want to return 1 on any pixel of the white field and 0 on any pixel of the black field. Looks simple but basic backpropagation seems to be useless for this task. It cannot be done by 1 layer network but also 2 layers is not enough. We must place hyperplanes between the fields (1 layer) and it reduces the problem to XOR (2 more layers). So we need 3 layers to compute this. And it is even worse, there is a pretty deep local minimum of the error function when we place hyperplanes in the first layer like in the basic XOR... Simple problem but hard for the backpropagation algorithm. The current version fails here but it is number one on my TODO list.
+
 </P>
 <UL>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.csv">Import the problem to be learned</A></LI>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.xml">Import the solution</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/chess/network.csv">Import the problem to be learned</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/chess/network.xml">Import the solution</A></LI>
 <LI><A HREF="network.csv">View the problem CSV file</A></LI>
 <LI><A HREF="network.xml">View the solution XML file</A></LI>
 <LI><BR><IMG SRC="result.png">
