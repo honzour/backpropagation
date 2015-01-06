66f06c456d32825427539e3d6176bc1013817ec3
Jan Nemec <honza@debian.honza>
2015-01-06 21:17:36 +0100

    more examples

--- web/2.0/examples/index.php
+++ web/2.0/examples/index.php
@@ -66,11 +66,18 @@ Simply return x + y. Learn from the table and generalize for non training set ex
 </P>
 </LI>
 <LI>
-<B><A HREF="plus">Distance</A></B><BR>
+<B><A HREF="distance">Distance</A></B><BR>
 <P>
 Calculate a distance from the point in 2D. Learn from the table and generalize for non training set examples.
 </P>
 </LI>
+<LI>
+<B><A HREF="sinus">Sinus</A></B><BR>
+<P>
+Learn a simple sinus wave from the training set. If it generalised right, it would generate a missing 2 pi period. But generalisation will fail here.
+</P>
+</LI>
+
 </UL>
 
 <?
