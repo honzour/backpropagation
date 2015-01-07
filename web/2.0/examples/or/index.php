93df19782532d939074e4c0fcc4a695cdf8b7f88
Jan Nemec <honza@debian.honza>
2015-01-07 05:57:20 +0100

    and and or

--- web/2.0/examples/or/index.php
+++ web/2.0/examples/or/index.php
@@ -1,19 +1,19 @@
 <HTML>
 <HEAD>
 <META charset="utf-8">
-<TITLE>Backpropagation - Examples - Identity</TITLE>
+<TITLE>Backpropagation - Examples - AND</TITLE>
 </HEAD>
 <BODY>
 <?
 include '../../google.php'
 ?>
-<H2>Identity</H2>
+<H2>AND</H2>
 <P>
-A very basic hello world example. We simply want the network to return 0 for input 0 and 1 for input 1. Single layer network with just one neuron is used and even this is quite overkill for this task.
+Compute the logical OR. This is very similar to previous example AND and again single layer network with just one neuron with two inputs is used.
 </P>
 <UL>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.csv">Import the problem to be learned</A></LI>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.xml">Import the solution</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/and/network.csv">Import the problem to be learned</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/and/network.xml">Import the solution</A></LI>
 <LI><A HREF="network.csv">View the problem CSV file</A></LI>
 <LI><A HREF="network.xml">View the solution XML file</A></LI>
 <LI><BR><IMG SRC="result.png">
