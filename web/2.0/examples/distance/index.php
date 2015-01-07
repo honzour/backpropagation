ff98c6b5bbe77f98886d564b32619032043605d2
Jan Nemec <honza@debian.honza>
2015-01-07 22:41:09 +0100

    distance

--- web/2.0/examples/distance/index.php
+++ web/2.0/examples/distance/index.php
@@ -1,19 +1,19 @@
 <HTML>
 <HEAD>
 <META charset="utf-8">
-<TITLE>Backpropagation - Examples - Identity</TITLE>
+<TITLE>Backpropagation - Examples - Distance</TITLE>
 </HEAD>
 <BODY>
 <?
 include '../../google.php'
 ?>
-<H2>Identity</H2>
+<H2>Distance</H2>
 <P>
-A very basic hello world example. We simply want the network to return 0 for input 0 and 1 for input 1. Single layer network with just one neuron is used and even this is quite overkill for this task.
+Calculate a distance from the point in 2D. Learn from the table and generalize for non training set examples.
 </P>
 <UL>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.csv">Import the problem to be learned</A></LI>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/identity/network.xml">Import the solution</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/distance/network.csv">Import the problem to be learned</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.0/examples/distance/network.xml">Import the solution</A></LI>
 <LI><A HREF="network.csv">View the problem CSV file</A></LI>
 <LI><A HREF="network.xml">View the solution XML file</A></LI>
 <LI><BR><IMG SRC="result.png">
