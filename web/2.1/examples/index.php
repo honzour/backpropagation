71aaf4acfbfaa1f971fcb955a9dfb0fe3405838c
Jan Nemec <honza@debian.honza>
2015-01-22 22:16:44 +0100

    examples update

--- web/2.1/examples/index.php
+++ web/2.1/examples/index.php
@@ -48,9 +48,21 @@ Return 1 near some point in 2D and 0 everywhere else. This is slightly more comp
 </P>
 </LI>
 <LI>
-<B><A HREF="chess">Chessboard 2x2</A></B><BR>
+<B><A HREF="chess">Chessboard 2x2 - 2-2-2-1</A></B><BR>
 <P>
-We have a chess board 2x2 and we want to return 1 on any pixel of the white field and 0 on any pixel of the black field. Looks but simple it seems to be a challenge for a basic backpropagation. It cannot be done by 1 layer network but also 2 layers is not enough. We must place hyperplanes between the fields (1 layer) and it reduces the problem to XOR (2 more layers). So we need 3 layers to compute this. And it is even worse, there is a pretty deep local minimum of the error function when we place hyperplanes in the first layer like in the basic XOR... Simple problem but hard for the backpropagation algorithm. The current version fails here many times. If you are lucky, you will get the good result but I would expect better behaviour. It is number one on my TODO list.
+We have a chess board 2x2 and we want to return 1 on any pixel of the white field and 0 on any pixel of the black field. Looks but simple it seems to be a challenge for a basic backpropagation. It cannot be done by 1 layer network but also 2 layers is not enough. We must place hyperplanes between the fields (1 layer) and it reduces the problem to XOR (2 more layers). So we need 3 layers to compute this. And it is even worse, there is a pretty deep local minimum of the error function when we place hyperplanes in the first layer like in the basic XOR... Simple problem but hard for the backpropagation algorithm. The current version fails here if the anatomy is 2-2-2-1, but there must be some way to teach the network algoritmically. It is on my TODO list.
+</P>
+</LI>
+<LI>
+<B><A HREF="chess2">Chessboard 2x2 - 2-4-2-1</A></B><BR>
+<P>
+The same but with more neurons in the first layer. Program sometimes sucseed to learn the training set.
+</P>
+</LI>
+<LI>
+<B><A HREF="chess3">Chessboard 2x2 - 2-4-2-1 - more elements in training set</A></B><BR>
+<P>
+The same but with larger training set.
 </P>
 </LI>
 <LI>
