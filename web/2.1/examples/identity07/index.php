b19cfaa64186c961c6e7c01eef50612dc9454b8c
Jan Nemec <honza@debian.honza>
2015-01-23 07:01:17 +0100

    identity07

---
+++ web/2.1/examples/identity07/index.php
@@ -0,0 +1,28 @@
+<HTML>
+<HEAD>
+<META charset="utf-8">
+<TITLE>Backpropagation - Examples - Identity on &lt;0, 7&gt;</TITLE>
+</HEAD>
+<BODY>
+<?
+include '../../google.php'
+?>
+<H2>Identity on &lt;0, 7&gt;</H2>
+<P>
+Return 0 for 0, 1 for 1, ... and 7 for 7. Just an identity on some interval. Network works well on this example.
+</P>
+<UL>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/identity/network.csv">Import the problem to be learned</A></LI>
+<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/identity/network.xml">Import the solution</A></LI>
+<LI><A HREF="network.csv">View the problem CSV file</A></LI>
+<LI><A HREF="network.xml">View the solution XML file</A></LI>
+<LI><BR><IMG SRC="result.png">
+<BR>Result visualisation</LI>
+<LI><BR><IMG SRC="network.png">
+<BR>Network visualisation</LI>
+</UL>
+<?
+include '../../links.php'
+?>
+</BODY>
+</HTML>
