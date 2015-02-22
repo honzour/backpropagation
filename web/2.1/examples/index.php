4ff7bf906bb2d3ea9da995451d138c794abfcff1
Jan Nemec <honza@debian.honza>
2015-02-22 05:38:36 +0100

    odd-even example - better description

--- web/2.1/examples/index.php
+++ web/2.1/examples/index.php
@@ -101,7 +101,7 @@ Calculate a distance of the point in 2D from [0.5, 0.5]. Learn from the table an
 <LI>
 <B><A HREF="oddeven">Odd - Even</A></B><BR>
 <P>
-Returns 1 for odd and 0 for even numbers. Training set is from 0 to 6 and the network can learn the training set pretty well. It fails for larger training set and it also fails to generalize for numbers that are not in the training set. Anyway, I would like to know how a common neural network with sigma activation fuction can generalize here...
+Returns 1 for odd and 0 for even numbers. The input is simply 1D, the real value of the number scaled to &lt;0, 1&gt;. Training set is from 0 to 6 and the network can learn the training set pretty well. It fails for larger training set and it also fails to generalize for numbers that are not in the training set. Anyway, I would like to know how a common neural network with sigma activation fuction can generalize here...
 </P>
 </LI>
 </UL>
--- web/2.1/examples/oddeven/index.php
+++ web/2.1/examples/oddeven/index.php
@@ -9,7 +9,7 @@ include '../../google.php'
 ?>
 <H2>Odd - Even</H2>
 <P>
-Returns 1 for odd and 0 for even numbers. Training set is from 0 to 6 and the network can learn the training set pretty well. It fails for larger training set and it also fails to generalize for numbers that are not in the training set. Anyway, I would like to know how a common neural network with sigma activation fuction can generalize here...
+Returns 1 for odd and 0 for even numbers. The input is simply 1D, the real value of the number scaled to &lt;0, 1&gt;. Training set is from 0 to 6 and the network can learn the training set pretty well. It fails for larger training set and it also fails to generalize for numbers that are not in the training set. Anyway, I would like to know how a common neural network with sigma activation fuction can generalize here...
 </P>
 <UL>
 <LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/oddeven/network.csv">Import the problem to be learned</A></LI>
