318ee581ea29fa0e1c948fdbacb4adae7a55da8b
Jan Nemec <honza@debian.honza>
2015-02-25 06:11:08 +0100

    shorter examples - better php

--- web/2.1/util.php
+++ web/2.1/util.php
@@ -26,21 +26,29 @@ function exampleText($title, $text)
 <?
 }
 
-function exampleCsvXmlPng()
+function exampleCsvXmlPngBool($showXml)
 {
 $root = "backpropagation://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]network.";
 ?>
 <UL>
-<LI><A HREF="<? echo $root; ?>csv">Import the problem to be learned</A></LI>
-<LI><A HREF="<? echo $root; ?>xml">Import the solution</A></LI>
-<LI><A HREF="network.csv">View the problem CSV file</A></LI>
-<LI><A HREF="network.xml">View the solution XML file</A></LI>
+<LI><A HREF="<? echo $root; ?>csv">Import the problem to be learned</A></LI><? 
+if ($showXml) { ?>
+<LI><A HREF="<? echo $root; ?>xml">Import the solution</A></LI><? } ?>
+<LI><A HREF="network.csv">View the problem CSV file</A></LI><? 
+if ($showXml) { ?>
+<LI><A HREF="network.xml">View the solution XML file</A></LI><? } ?>
 <LI><BR><IMG SRC="result.png">
 <BR>Result visualisation</LI>
 <LI><BR><IMG SRC="network.png">
 <BR>Network visualisation</LI>
 </UL>
 <?
+}
+
+
+function exampleCsvXmlPng()
+{
+  exampleCsvXmlPngBool(true);
 }
 
 function exampleFooter()
@@ -59,6 +67,15 @@ function example($title, $text)
   exampleGoogle();
   exampleText($title, $text);
   exampleCsvXmlPng();
+  exampleFooter();
+}
+
+function exampleBool($title, $text, $showXml)
+{
+  exampleHeader($title);
+  exampleGoogle();
+  exampleText($title, $text);
+  exampleCsvXmlPngBool($showXml);
   exampleFooter();
 }
 
