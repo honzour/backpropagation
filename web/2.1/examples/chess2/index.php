049fce2927903973ba43aa519945b1a0e1b0563d
Jan Nemec <honza@debian.honza>
2015-02-25 05:53:29 +0100

    shorter examples - better php

--- web/2.1/examples/chess2/index.php
+++ web/2.1/examples/chess2/index.php
@@ -1,27 +1,5 @@
-<HTML>
-<HEAD>
-<META charset="utf-8">
-<TITLE>Backpropagation - Examples -  Chessboard 2x2 - 2-4-2-1</TITLE>
-</HEAD>
-<BODY>
 <?
-include '../../google.php'
+include '../../util.php';
+example("Chessboard 2x2 - 2-4-2-1", "Like the previous example but with more neurons in the first layer. Program sometimes sucseed to learn the training set.");
 ?>
-<H2>Chess 2x2 - 2-4-2-1</H2>
-<P>
-Like the previous example but with more neurons in the first layer. Program sometimes sucseed to learn the training set.</P>
-<UL>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/chess2/network.csv">Import the problem to be learned</A></LI>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/chess2/network.xml">Import the solution</A></LI>
-<LI><A HREF="network.csv">View the problem CSV file</A></LI>
-<LI><A HREF="network.xml">View the solution XML file</A></LI>
-<LI><BR><IMG SRC="result.png">
-<BR>Result visualisation</LI>
-<LI><BR><IMG SRC="network.png">
-<BR>Network visualisation</LI>
-</UL>
-<?
-include '../../links.php'
-?>
-</BODY>
-</HTML>
+
--- web/2.1/examples/chess3/index.php
+++ web/2.1/examples/chess3/index.php
@@ -1,28 +1,5 @@
-<HTML>
-<HEAD>
-<META charset="utf-8">
-<TITLE>Backpropagation - Examples - Chessboard 2x2 - 2-4-2-1 - more elements in the training set</TITLE>
-</HEAD>
-<BODY>
 <?
-include '../../google.php'
+include '../../util.php';
+example("Chessboard 2x2 - 2-4-2-1 - more elements in the training set", "Like the previous example but with larger training set. The network sometimes learn the task well and sometimes not.");
 ?>
-<H2>Chessboard 2x2 - 2-4-2-1 - more elements in the training set</H2>
-<P>
-Like the previous example but with larger training set. The network sometimes learn the task well and sometimes not.
-</P>
-<UL>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/chess3/network.csv">Import the problem to be learned</A></LI>
-<LI><A HREF="backpropagation://backpropagation.moxo.cz/2.1/examples/chess3/network.xml">Import the solution</A></LI>
-<LI><A HREF="network.csv">View the problem CSV file</A></LI>
-<LI><A HREF="network.xml">View the solution XML file</A></LI>
-<LI><BR><IMG SRC="result.png">
-<BR>Result visualisation</LI>
-<LI><BR><IMG SRC="network.png">
-<BR>Network visualisation</LI>
-</UL>
-<?
-include '../../links.php'
-?>
-</BODY>
-</HTML>
+
