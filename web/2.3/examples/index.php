1f3bf4851a83aa5b6bd3ca6f5323ec790036140c
Jan Nemec <jnemec@acrobits.cz>
2015-04-28 10:58:23 +0200

    updated sinus2 text

--- web/2.3/examples/index.php
+++ web/2.3/examples/index.php
@@ -125,7 +125,7 @@ In this cases neural network should predict the next value(s) from previous valu
 <LI>
 <B><A HREF="sinus2">Sinus sequence</A></B><BR>
 <P>
-Training set is a sequence of sinus(k * 0.1) and the network task is to predict the next value from previous two. So for example if you put sinus(0.05) and sinus(0.15), you should get sinus(0.25). It works well..
+Training set is a sequence of sinus(k * 0.1) and the network task is to predict the next value from previous two. So for example if you put sinus(0.05) and sinus(0.15), you should get sinus(0.25). The first predicted values are ok, but predicting sinus forever does not work.
 </P>
 </LI>
 </UL>
--- web/2.3/examples/sinus2/index.php
+++ web/2.3/examples/sinus2/index.php
@@ -1,5 +1,5 @@
 <?
 include '../../util.php';
-example("Sinus sequence", "Training set is a sequence of sinus(k * 0.1) and the network task is to predict the next value from previous two. So for example if you put sinus(0.05) and sinus(0.15), you should get sinus(0.25). It works well.");
+example("Sinus sequence", "Training set is a sequence of sinus(k * 0.1) and the network task is to predict the next value from previous two. So for example if you put sinus(0.05) and sinus(0.15), you should get sinus(0.25). The first predicted values are ok, but predicting sinus forever does not work.");
 ?>
 
