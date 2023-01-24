ed620fa0b645eff09e8114ccf634cb5611eb22e1
honzour <jnem6403@seznam.cz>
2023-01-24 03:13:42 +0100

    removed old directory

--- backpropagation/src/cz/honza/backpropagation/network/trainingset/TrainingLine.java
+++
@@ -1,8 +0,0 @@
-package cz.honza.backpropagation.network.trainingset;
-
-import java.io.Serializable;
-
-public interface TrainingLine extends Serializable {
-	public String toString();
-	public String getEditorType();
-}
